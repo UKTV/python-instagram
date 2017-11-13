68c4dec2532b5c2ec5eb3d23a839aba8c898fcb5
Ryan Stalbow <ryan.stalbow@uktv.co.uk>
2017-11-13 10:28:29 +0000

    fixes pulled in from forked repos

--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,4 @@
-from .helper import timestamp_to_datetime
-import six
+from helper import timestamp_to_datetime
 
 
 class ApiModel(object):
@@ -10,20 +9,15 @@ class ApiModel(object):
         if entry is None:
             return ""
         entry_str_dict = dict([(str(key), value) for key, value in entry.items()])
-        return cls(**entry_str_dict)
+        object = cls(**entry_str_dict)
+        object._api_dict = entry
+        return object
+    
+    def object_to_dictionary(self):
+        return self._api_dict
 
     def __repr__(self):
-        return str(self)
-        # if six.PY2:
-        #     return six.text_type(self).encode('utf8')
-        # else:
-        #     return self.encode('utf8')
-
-    def __str__(self):
-        if six.PY3:
-            return self.__unicode__()
-        else:
-            return unicode(self).encode('utf-8')
+        return unicode(self).encode('utf8')
 
 
 class Image(ApiModel):
@@ -39,6 +33,13 @@ class Image(ApiModel):
 
 class Video(Image):
 
+    #2017-09-27: Video media type returns a new kwarg: id
+    #def __init__(self, url, width, height):
+    def __init__(self, *args, **kwargs):
+        for k,v in kwargs.iteritems():
+            setattr(self,k,v)
+        super(Image, self).__init__(*args)
+
     def __unicode__(self):
         return "Video: %s" % self.url
 
@@ -47,7 +48,7 @@ class Media(ApiModel):
 
     def __init__(self, id=None, **kwargs):
         self.id = id
-        for key, value in six.iteritems(kwargs):
+        for key, value in kwargs.iteritems():
             setattr(self, key, value)
 
     def get_standard_resolution_url(self):
@@ -73,17 +74,20 @@ class Media(ApiModel):
     @classmethod
     def object_from_dictionary(cls, entry):
         new_media = Media(id=entry['id'])
+        new_media._api_dict = entry
         new_media.type = entry['type']
 
         new_media.user = User.object_from_dictionary(entry['user'])
 
         new_media.images = {}
-        for version, version_info in six.iteritems(entry['images']):
+        #2017-02-23: http://stackoverflow.com/questions/42456908/instagram-api-missing-elements-in-json-response
+        for version, version_info in entry.get('images',{}).iteritems():
             new_media.images[version] = Image.object_from_dictionary(version_info)
 
         if new_media.type == 'video':
             new_media.videos = {}
-            for version, version_info in six.iteritems(entry['videos']):
+            #2017-02-23: http://stackoverflow.com/questions/42456908/instagram-api-missing-elements-in-json-response
+            for version, version_info in entry.get('videos',{}).iteritems():
                 new_media.videos[version] = Video.object_from_dictionary(version_info)
 
         if 'user_has_liked' in entry:
@@ -96,26 +100,27 @@ class Media(ApiModel):
 
         new_media.comment_count = entry['comments']['count']
         new_media.comments = []
-        if 'data' in entry['comments']:
-            for comment in entry['comments']['data']:
-                new_media.comments.append(Comment.object_from_dictionary(comment))
+        #HOTFIX
+        #for comment in entry['comments']['data']:
+        for comment in entry['comments'].get('data',[]):
+            new_media.comments.append(Comment.object_from_dictionary(comment))
 
+        new_media.created_time = timestamp_to_datetime(entry['created_time'])
+        
         new_media.users_in_photo = []
         if entry.get('users_in_photo'):
             for user_in_photo in entry['users_in_photo']:
                 new_media.users_in_photo.append(UserInPhoto.object_from_dictionary(user_in_photo))
-
-        new_media.created_time = timestamp_to_datetime(entry['created_time'])
-
+        
         if entry['location'] and 'id' in entry:
             new_media.location = Location.object_from_dictionary(entry['location'])
 
         new_media.caption = None
         if entry['caption']:
             new_media.caption = Comment.object_from_dictionary(entry['caption'])
-        
-        new_media.tags = []
+
         if entry['tags']:
+            new_media.tags = []
             for tag in entry['tags']:
                 new_media.tags.append(Tag.object_from_dictionary({'name': tag}))
 
@@ -130,14 +135,14 @@ class MediaShortcode(Media):
 
     def __init__(self, shortcode=None, **kwargs):
         self.shortcode = shortcode
-        for key, value in six.iteritems(kwargs):
+        for key, value in kwargs.iteritems():
             setattr(self, key, value)
 
 
 class Tag(ApiModel):
     def __init__(self, name, **kwargs):
         self.name = name
-        for key, value in six.iteritems(kwargs):
+        for key, value in kwargs.iteritems():
             setattr(self, key, value)
 
     def __unicode__(self):
@@ -146,12 +151,13 @@ class Tag(ApiModel):
 
 class Comment(ApiModel):
     def __init__(self, *args, **kwargs):
-        for key, value in six.iteritems(kwargs):
+        for key, value in kwargs.iteritems():
             setattr(self, key, value)
 
     @classmethod
     def object_from_dictionary(cls, entry):
         user = User.object_from_dictionary(entry['from'])
+        user._api_dict = entry
         text = entry['text']
         created_at = timestamp_to_datetime(entry['created_time'])
         id = entry['id']
@@ -172,8 +178,8 @@ class Point(ApiModel):
 
 class Location(ApiModel):
     def __init__(self, id, *args, **kwargs):
-        self.id = str(id)
-        for key, value in six.iteritems(kwargs):
+        self.id = id
+        for key, value in kwargs.iteritems():
             setattr(self, key, value)
 
     @classmethod
@@ -185,6 +191,7 @@ class Location(ApiModel):
         location = Location(entry.get('id', 0),
                        point=point,
                        name=entry.get('name', ''))
+        location._api_dict = entry
         return location
 
     def __unicode__(self):
@@ -195,7 +202,7 @@ class User(ApiModel):
 
     def __init__(self, id, *args, **kwargs):
         self.id = id
-        for key, value in six.iteritems(kwargs):
+        for key, value in kwargs.iteritems():
             setattr(self, key, value)
 
     def __unicode__(self):
@@ -216,6 +223,7 @@ class Relationship(ApiModel):
         return "Relationship: (Follows: %s, Followed by: %s)" % (follows, followed)
 
 
+
 class Position(ApiModel):
     def __init__(self, x, y):
         self.x = x
@@ -227,7 +235,9 @@ class Position(ApiModel):
     @classmethod
     def object_from_dictionary(cls, entry):
         if 'x' in entry:
-            return Position(entry['x'], entry['y'])
+            position = Position(entry['x'], entry['y'])
+            position._api_dict = entry
+            return position
 
 
 class UserInPhoto(ApiModel):
@@ -246,5 +256,6 @@ class UserInPhoto(ApiModel):
 
         if 'position' in entry:
             position = Position(entry['position']['x'], entry['position']['y'])
-
-        return UserInPhoto(user, position)
+        user_in_photo = UserInPhoto(user, position)
+        user_in_photo._api_dict = entry
+        return user_in_photo
